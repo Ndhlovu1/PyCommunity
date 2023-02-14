c30528861da363f17b9ac4e9bacd79d999504abc
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-02-14 22:22:51 +0200

    Added the STATICFILES_DIRS

--- PyCommunityProject/PyCommunityProject/settings.py
+++ PyCommunityProject/PyCommunityProject/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'channels',
+    'pycomDashboard',
+   
 ]
 
 MIDDLEWARE = [
@@ -116,6 +119,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    "pycomDashboard/static",
+]
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
